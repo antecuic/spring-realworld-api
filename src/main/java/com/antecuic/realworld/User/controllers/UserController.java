af907aae640b5f55e0a65b189d7c41fec89fd3bb
Ante Čuić <antecuic@Antes-MacBook-Pro.local>
2024-01-08 17:28:13 +0100

    removed not used route

--- src/main/java/com/antecuic/realworld/User/controllers/UserController.java
+++ src/main/java/com/antecuic/realworld/User/controllers/UserController.java
@@ -13,9 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController()
 @RequestMapping("/users")
@@ -35,11 +32,6 @@ public class UserController {
     public String login(@RequestBody LoginCredentialsDTO credentials) {
         return userService.authenticate(credentials);
 
-    }
-
-    @GetMapping("/{email}")
-    public UserDTO getUserByEmail(@PathVariable String email) {
-        return userService.getUserByEmailOrUsername(email, null);
     }
 
 }
